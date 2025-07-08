3336f55b0ab8deb04a50ba2d1d94cba6162fb4df
Arthur Erdös <a.erdoes@gmail.com>
2025-07-08 13:26:43 +0200

    be: 9th commit - add user id to success callback url

--- sy-stripe-service/internal/app/handlers/checkout_handler.go
+++ sy-stripe-service/internal/app/handlers/checkout_handler.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"net/http"
+	"strings"
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 	"sy-stripe-service/internal/app/services"
@@ -48,7 +49,17 @@ func (h *CheckoutHandler) CreateCheckoutSessionHandler(c *gin.Context) {
 		return
 	}
 
-	session, err := h.Service.CreateCheckoutSession(req.PriceID, userIDPtr, h.SuccessURL, h.CancelURL)
+	// Parametrize success URL with internal user ID if present
+	successURL := h.SuccessURL
+	if userIDPtr != nil {
+		sep := "?"
+		if strings.Contains(successURL, "?") {
+			sep = "&"
+		}
+		successURL = successURL + sep + "user_id=" + userIDPtr.String()
+	}
+
+	session, err := h.Service.CreateCheckoutSession(req.PriceID, userIDPtr, successURL, h.CancelURL)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
